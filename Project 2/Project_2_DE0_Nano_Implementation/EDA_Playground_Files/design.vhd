f0351402d0eab8b407adf3922e8cce72a473e71c
kylebielby <kbielby09@gmail.com>
2020-09-09 16:11:23 -0400

    modified state machine code for actual inputs

--- Project 2/Project_2_DE0_Nano_Implementation/EDA_Playground_Files/design.vhd
+++ Project 2/Project_2_DE0_Nano_Implementation/EDA_Playground_Files/design.vhd
@@ -14,79 +14,64 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.Numeric_std.all;
 
--- Parking lot counter 
-entity carCounter is 
-	port(clk, reset, forward, backward: in Std_logic;
-    	output: out Std_logic;
-        carCount: out Std_logic_Vector(7 downto 0)) 
+-- Parking lot counter
+entity carCounter is
+	port(clk, reset, sensorA, sensorB: in Std_logic;
+        carCount: out Std_logic_Vector(7 downto 0))
 end;
 
 -- Define Behavior of Testbench
 architecture behavior of carCounter is
 	-- define states for carCounter FSM
     type States is (A, B, BOTH, IDLE);
-	signal state : States; 
+	signal state : States;
     signal previousState : States;
     signal count : unsigned(7 downto 0) := "00000000";
-    
+
 	begin
-    
+
     --sequential behavior of car counter
     process(clk, reset, forward, backward) begin
-    	
-        --reset car counter to 0 
-    	if reset = '1' then 
+
+        --reset car counter to 0
+    	if reset = '1' then
         	--reset binary count to 0
         	carCount <= "00000000";
             count <= "00000000";
-            
+
         --check for a change of states when clock changes
-        elsif rising_edge(clk) then 
-        	case state is
-            	when A=>
-                	if forward = '1' then
-                    	--update previous state
-                        previousState <= state;
-                    	--car moves forward to state BOTH
-                        state <= B;                        
-                    elsif backward = '1' then
-                    	--update previous state
-                        previousState <= state;
-                        state <= IDLE;                    
-                    end if;
-                    
-                when B=>
-                	if forward = '1' then
-                    	previousState <= state;
-                        state <= IDLE;
-                    elsif backward = '1' then
-                    	previousState <= state;
-                        state <= BOTH;
-                    end if;
-                    
-                when BOTH=>
-                	if forward = '1' then
-                    	previousState <= state;
-                        state <= B;
-                    elsif backward = '1' then
-                    	previousState <= state;
-                        state <= A;
-                    end if;
-                    
-                when IDLE=>
-                	if forward = '1' then
-                    	previousState <= state;
-                        state <= A;
-                    elsif backward = '1' then
-                    	previousState <= state;
-                        state <= B;
-                    end if;
-			end case;
+        elsif rising_edge(clk) then
+        	if sensorA = '1' then
+            	--update previous state
+                previousState <= state;
+                --check if both inputs are on
+                if sensorB = '1' then
+                	state <= BOTH;
+                else
+                	state <= A;
+                end if;
+			end if;
+
+            --check for state transition to B
+            if sensorB = '1' then
+            	previousState <= state;
+            	if A = '0' then
+                	state <= B;
+                end if;
+            end if;
+
+            --state transition to IDLE
+            if sensorA = '0' then
+            	if sensorB ='0' then
+                	state <= IDLE;
+                end if;
+            end if;
+
         end if;
-        
+
     end process;
-    
-    process(state)
+
+    process(state, previousState)
     	begin
         -- update count when in propper states
         case state is
@@ -96,11 +81,13 @@ architecture behavior of carCounter is
                     count <= count + 1;
                 end if;
 			when IDLE=>
-            	if previousState = B then 
+            	if previousState = B then
                 	--increment count
                     count <= count + 1;
                 end if;
         end case;
-        
+
+        --Send vehicle count to output
+        carCount <= Std_logic_vector(count);
     end process;
 end;
